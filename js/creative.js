605ed3d3335d6f8b69497d71fc560c4c9c4f124d
Umesh Kumar <umeshkumarkhanna@hotmail.com>
2016-02-09 19:32:48 -0500

    initial commit

---
+++ js/creative.js
@@ -0,0 +1,97 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+})(jQuery); // End of use strict
+
+
+var startY = 100;
+var stopY = 18000;
+
+$(window).scroll(function(){
+    checkY();
+});
+
+function checkY()
+{
+    console.log($(window).scrollTop()); 
+    if($(window).scrollTop() > startY && $(window).scrollTop() <= stopY)
+    {
+        console.log("Show"); 
+
+        $('.foxedDiv').show(); 
+    }
+    else
+    {
+        console.log("Hide"); 
+        $('.foxedDiv').hide();
+    }
+}
+
+checkY();
+
+
+
+
+var downloadLink = document.getElementById('subscribeButton');
+addListener(downloadLink, 'click', function() {
+  ga('send', 'event', 'button', 'click', 'nav-buttons');
+});
+
+
+/**
+ * Utility to wrap the different behaviors between W3C-compliant browsers
+ * and IE when adding event handlers.
+ *
+ * @param {Object} element Object on which to attach the event listener.
+ * @param {string} type A string representing the event type to listen for
+ *     (e.g. load, click, etc.).
+ * @param {function()} callback The function that receives the notification.
+ */
+function addListener(element, type, callback) {
+ if (element.addEventListener) element.addEventListener(type, callback);
+ else if (element.attachEvent) element.attachEvent('on' + type, callback);
+}
